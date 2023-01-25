74bedfc480565e7522e0920fc4baf4624d5060ab
jinho <36149614+dazzaguzza@users.noreply.github.com>
2023-01-25 18:54:11 +0900

    ListAdapter(RecyclerView) with databinding , Room search

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -1,12 +1,23 @@
 package com.codinginflow.mvvmtodo.ui.tasks
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val  tasks = tasksFlow.asLiveData()
 }
